0f1969444287900907dfa48dc2f137741591e184
Marten Jacobs <martenjacobs@users.noreply.github.com>
2021-01-21 11:12:25 +0100

    Update run.sh

--- restic/run.sh
+++ restic/run.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-CONFIG_PATH=/data/options.json
-
+# find and mount the data partition
 udevd --daemon && sleep 0.5
 udevadm trigger && sleep 0.5
 mkdir /hassos-data
 mount /dev/disk/by-label/hassos-data /hassos-data/
 
+# load configuration
+CONFIG_PATH=/data/options.json
+$(jq -r \
+  '.env_vars|to_entries[]|"export " + .key + "=" + (.value|tostring) + ""' \
+  "$CONFIG_PATH")
+
+# run the backup
 restic --verbose backup /backup-source/ || true
 
 # Temporary ssh related stuff
