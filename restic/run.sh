6b7bb808300760e81af082c9531824accd0aca6e
Marten Jacobs <martenjacobs@users.noreply.github.com>
2021-01-21 12:39:00 +0100

    Update run.sh

--- restic/run.sh
+++ restic/run.sh
@@ -15,7 +15,9 @@ CONFIG_PATH=/data/options.json
 $(jq -r \
   '.env_vars|to_entries[]|"export " + .key + "=" + (.value|tostring) + ""' \
   "$CONFIG_PATH")
-
+jq -r \
+  '.exclude_patterns[]|.' \
+  "$CONFIG_PATH" > /exclude_file
 # run the backup
 (
   cd /hassos-data/supervisor
