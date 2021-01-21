fcdec3746ff5f8bb77ce48a89617b24439a8c201
Marten Jacobs <martenjacobs@users.noreply.github.com>
2021-01-21 11:49:37 +0100

    Update run.sh

--- restic/run.sh
+++ restic/run.sh
@@ -18,32 +18,4 @@ $(jq -r \
 (
   cd /hassos-data/supervisor
   restic --verbose backup .
-)|| true
-# Temporary ssh related stuff
-echo "root:password" | chpasswd
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-	# generate fresh rsa key
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-fi
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-	# generate fresh dsa key
-	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-fi
-
-#prepare run dir
-if [ ! -d "/var/run/sshd" ]; then
-  mkdir -p /var/run/sshd
-fi
-
-#cat /etc/ssh/sshd_config | grep "^[^#]"
-
-cat <<EOF > /etc/ssh/sshd_config
-PermitRootLogin yes
-AuthorizedKeysFile	.ssh/authorized_keys
-AllowTcpForwarding no
-GatewayPorts no
-X11Forwarding no
-Subsystem	sftp	/usr/lib/ssh/sftp-server
-EOF
-
-/usr/sbin/sshd -D
+)
